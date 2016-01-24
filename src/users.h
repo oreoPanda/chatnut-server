508e092c1dec43a4fbc52b7e8dc8760c9033ce2e
Jonas <legojonas@gmx.de>
2016-01-24 16:33:32 +0100

    Added users.c and users.h

---
+++ src/users.h
@@ -0,0 +1,12 @@
+/*server.h*/
+
+#ifndef USERS_H
+#define USERS_H
+
+#define USERFILE "users"
+
+extern int login( char *username, char *password );
+extern int find_user( char *username, int *index );
+extern int add_user( char *username, char *password );
+
+#endif	//USERS_H
