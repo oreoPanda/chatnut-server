a503840779871aa89d79e475a75228ec4a3d487b
Jonas <legojonas@gmx.de>
2016-01-24 17:20:02 +0100

    Added function to create the server's directory structure

--- src/users.h
+++ src/users.h
@@ -5,6 +5,7 @@
 
 #define USERFILE "users"
 
+extern int init_server_directory(void);
 extern int login( char *username, char *password );
 extern int find_user( char *username, int *index );
 extern int add_user( char *username, char *password );
