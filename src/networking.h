94ee081370cb6b4b3916631c08747e96547c089c
Jonas <legojonas@gmx.de>
2016-06-07 17:16:50 +0200

    Added the GPL license notices to the top of each source file and header

--- src/networking.h
+++ src/networking.h
@@ -1,9 +1,21 @@
-/*
- * Client.h
- *
- *  Created on: Feb 7, 2016
- *      Author: i_fly_gliders
- */
+/*Client.h*/
+
+/*Copyright (C) 2016 Jonas Fuglsang-Petersen*/
+
+/*This file is part of chatnut-server.*/
+
+/*chatnut-server is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+chatnut-server is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with chatnut.  If not, see <http://www.gnu.org/licenses/>.*/
 
 #ifndef NETWORKING_H_
 #define NETWORKING_H_
